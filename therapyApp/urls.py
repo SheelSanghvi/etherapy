bc3c02bc624a2dd2b4233652ca346abe2441e789
SheelSanghvi <sanghvisheel@gmail.com>
2019-10-04 22:28:30 +0530

    task 1

--- therapyApp/urls.py
+++ therapyApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-	path('',views.index, name='index'),path('add/<int:a>/<int:b>/',views.add, name='add')
+	path('',views.index, name='index'),path('add/<int:a>/<int:b>/',views.add, name='add'), path('login/<str:username>/',
+	views.login, name='login')
 ]
--- therapyApp/views.py
+++ therapyApp/views.py
@@ -4,5 +4,7 @@ def index(request):
 	return HttpResponse("Hello, World. You're at polls index.")
 def add(request,a,b):
 	return HttpResponse("sum is {}".format(a+b))
+def login(request,username):
+	return HttpResponse("you have logged in successfully in account: {}".format(username))
 	
 # Create your views here.
